41969c1a9de7fa74293caa830b73b49bf3273313
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-01-26 12:41:23 -0800

    Update 3_LargestPrimeFactor.py

--- 3_LargestPrimeFactor.py
+++ 3_LargestPrimeFactor.py
@@ -1,5 +1,29 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 
 #What is the largest prime factor of the number 600851475143 ?
+import math 
+  
+def x(n):
+    factors = []
+    for i in range(3,int(math.sqrt(n))):
+        if(n%i==0):
+            factors.append(i)
+            
+    return factors
+    
+composite=1
+for number in x(600851475143):
+    #print(number)
+    if(composite < 600851475143):
+        print(number)
+        composite=composite*number
+
+print(composite)
+
+#Now to prove that 6857 is prime
+if not x(6857):
+    print("It is prime")
+else:
+    print("Not prime")
 
 
